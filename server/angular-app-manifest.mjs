b1c1abe8c421747aec9db4097c737ce944fd425b
Logan Ingram <loganphx1@gmail.com>
2025-08-18 09:53:04 -0700

    Updated portfolio section

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -17,9 +17,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11516, hash: '9d0e6de8912114c02f4176a924eca6c1006d67ddcb1a1e6600274f33659bbaea', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1006, hash: '5b4939171f07ca0e7d8b9e5b1a5a2b95a4d4fe641d325e04a137fed69ca5a79f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 35581, hash: 'f3bb0e01f18673ade2effdb4cb449b12f83c7a4bc0e2bf8f672af4a273b3dd55', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11516, hash: '45d177009675524b501e735423d45629dabb8817e326f5a159fd4f85b980d312', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1006, hash: '0a66a68baa2a2a1f041ba5f9adbd638fec6b9503c3afaa5234a6def1cb055384', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 36168, hash: '916b3d0b3b7d57cbb78eb2cfb62be207232d788a6504e0ed825c1d8ec66a027c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-FX5M6IIE.css': {size: 15531, hash: '66mIG05Mmc8', text: () => import('./assets-chunks/styles-FX5M6IIE_css.mjs').then(m => m.default)}
   },
 };
